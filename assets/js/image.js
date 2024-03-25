1698a0af08a6b90d42e182d0c698a72e481c0f89
zlucasftw <ilucasmcpvp@gmail.com>
2024-03-25 00:45:33 -0300

    First Commit Moment :)

---
+++ assets/js/image.js
@@ -0,0 +1,12 @@
+import '../json/images.json';
+
+/* foreach (id =>) */
+/* document.write('
+<div class="imagem_grid">
+        <figure>
+            <img src="assets/img/galaxia.jpg" alt="" width="">
+            <figcaption></figcaption>
+        </figure> 
+        <img src="assets/img/essay-gettyimages-1237093074.webp" alt="BLIND IS YOU?">
+    </div>
+'); */
